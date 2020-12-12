e27481f28697f61c7a612d4ab6269248eead543e
Alejandro Sanchez <alejandrosanchezwe@outlook.com>
2020-12-11 23:19:37 -0600

    updated api routes

--- server.js
+++ server.js
@@ -20,15 +20,15 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/tracker", {
 // routes
 
 app.get("/", function(req, res) {
-res.send("Fitness Tracker!");
+res.send(path.join(__dirname,"/public/index.html"));
 });
 
 app.get("/stats", function(req, res) {
-res.send("This is the stats page");
+res.send(path.join(__dirname,"/public/stats.html"));
 });
 
 app.get("/exercise", function(req, res) {
-res.send("exercise page");
+res.send(path.join(__dirname,"/public/exercise.html"));
 });
 
 app.listen(PORT, () => {
